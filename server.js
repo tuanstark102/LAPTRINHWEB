683c7d1684842d25714e02aed64effe0b3607a62
BachAn1205 <148757605+BachAn1205@users.noreply.github.com>
2024-11-30 11:08:52 +0700

    server.js

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
-const express = require('express');
-const app = express();
-const PORT = 3000;
+console.log("hello");
+/* Khai báo biến let , var , const */
 
-app.use(express.static('public'));
+let a = 7;
+console.log(`Gia tri ban dau a = ${a}`);
+ 
+a = a + 3 ;
+console.log(`Gia tri sau khi thay doi a = ${a}`);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
-});
+/* String */
+let namee = "BachAn";
+console.log(namee.at(3))
+console.log(`Gia tri ban dau ${namee} , sau khi dung namee.toLowerCase() = `)
+console.log(namee.toLowerCase()) 
+console.log(namee.endsWith("a")) /* false */
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+/* reguler express trong tri tue nhan tao */
+
+console.log(namee.slice(2, 4)) ;/* an */ /* Cắt từ vị trí thứ 2 đến thứ 4 */ 
+console.log(namee.substring(1,2)); 
+
+age = 19;
+console.log(age++); /* 19 */
+console.log(age); /* 20 */
+console.log(++age); /* 21 */
+
+n = 3 /* 0011 */
+console.log(n<<1)  /* 0 0110 = 6 */
+
+n = 23 /* 0001  */
+console.log(n<<1) /* 14 */
+
+/* Nhập vào từ người dùng và in ra 1 nội dung nào đó */
+let weight = prompt("Nhap vao can nang cua ban : ")
+let height = prompt("Nhap vao chieu cao cua ban : ")
+console.log(`Chi so BMI cua ban la : ${height/weight}`)
